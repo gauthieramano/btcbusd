6d8150ec31c2300c54e4b11aacc4b58c86158428
Gauthier Amano <gauthier.net@gmail.com>
2025-03-13 08:41:43 +0100

    feat(deno): ✨ log the high, price & low every second

--- deno/main.ts
+++ deno/main.ts
@@ -1,4 +1,12 @@
-const ws = new WebSocket("wss://stream.binance.com:9443/ws/btcbusd@trade");
+let ws = new WebSocket("wss://stream.binance.com:9443/ws/btcfdusd@trade");
+
+const lows = [...new Array(6)].fill(200000);
+const highs = [...new Array(6)].fill(0);
+let prevIndex = 0;
+let time = 0;
+
+const pad = (value: string) => value.padStart(7, " ");
+const format = (value: number) => pad(new Intl.NumberFormat().format(value));
 
 ws.onmessage = (event) => {
   /**
@@ -18,16 +26,49 @@ ws.onmessage = (event) => {
    * }
    * ```
    */
-  const { E, p, q } = JSON.parse(event.data);
+  // const { E, p, q } = JSON.parse(event.data);
 
-  const date = new Date(E).toUTCString();
-  const price = parseFloat(p).toFixed(2);
-  const quantity = q;
+  // const date = new Date(E).toUTCString();
+  // const price = parseFloat(p).toFixed(0);
+  // const quantity = q;
 
   // {
   //   date: 'Fri, 19 May 2023 23:01:37 GMT',
   //   price: '26875.95',
   //   quantity: '0.00102000'
   // }
-  console.log({ date, price, quantity });
+
+  //console.log({ date, price, quantity });
+
+  const { E, p } = JSON.parse(event.data);
+
+  const price = parseInt(p);
+  const index = Math.floor((E % 60000) / 10000);
+  const isNewIndex = prevIndex !== index;
+
+  if (isNewIndex) {
+    prevIndex = index;
+  }
+
+  if (isNewIndex || price < lows[index]) {
+    lows[index] = price;
+  }
+  if (isNewIndex || price > highs[index]) {
+    highs[index] = price;
+  }
+
+  if (E > time) {
+    time = E + 1000;
+
+    const low = Math.min(...lows);
+    const high = Math.max(...highs);
+
+    const prices = [
+      `${format(high)} - ${high - price}`,
+      format(price),
+      `${format(low)} - ${price - low}`,
+    ].join(" \n");
+
+    console.log(prices);
+  }
 };
